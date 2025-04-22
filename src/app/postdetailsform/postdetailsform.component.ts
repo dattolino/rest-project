0b58aa72f563d259a042ef02225eda29bc8e9078
Your Name <you@example.com>
2025-04-22 15:40:06 +0200

    importato select module

--- src/app/postdetailsform/postdetailsform.component.ts
+++ src/app/postdetailsform/postdetailsform.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatInput, MatFormField, MatError, MatLabel } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @Component({
   selector: 'app-postdetailsform',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButton,
+    MatInput,
+    MatFormField,
+    MatError,
+    MatLabel,
+    MatSelectModule
+  ],
   templateUrl: './postdetailsform.component.html',
   styleUrl: './postdetailsform.component.css'
 })
